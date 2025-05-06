f31d551d0c22fdf41f04139a4105a249bf4f8e9e
Hooria-Ali-12 <bscs2312419@szabist.pk>
2025-05-06 12:57:02 +0000

    all

---
+++ Studentweb/js/count.js
@@ -0,0 +1,7 @@
+const count_dept =" ";
+const count_emp =" ";
+const count_job_history= " ";
+const count_jobs= " ";
+const count_location = "";
+const count_region = "";
+const count_country ="";
---
+++ Studentweb/js/jobscript.js
@@ -0,0 +1,23 @@
+const Job_API_LINK= "https://legendary-invention-7vwjg4p5jvqq2rq96-5001.app.github.dev/jobs"
+
+
+fetch(Job_API_LINK).then(response=>{
+    if(!response.ok)
+        throw new Error("Failed to fetch data");
+    return response.json();
+}).then(data=>{
+    const tbody = document.querySelector("#jobtable tbody")
+    data.forEach(jobs=>{
+        const row = document.createElement("tr");
+        row.innerHTML = `
+         <td> ${jobs.job_id} </td> 
+          <td> ${jobs.job_title} </td> 
+            <td> ${jobs.max_salary} </td>
+             <td> ${jobs.min_salary} </td>  `;
+        
+            tbody.appendChild(row);
+    });
+
+}).catch(err=>{
+    console.log(err.message);
+});
